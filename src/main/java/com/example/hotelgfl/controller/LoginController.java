89afc8170ccbcb7869edeae9f9aafcc4f67b6a72
nikita <nikitamazurenko42@gmail.com>
2023-09-06 22:31:08 +0300

    Return User after successful authentication

---
+++ src/main/java/com/example/hotelgfl/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.example.hotelgfl.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @PostMapping
+    public ResponseEntity<User> authenticate() {
+        User auth = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.ok(auth);
+    }
+}
